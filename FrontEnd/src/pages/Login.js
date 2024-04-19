151a61c13a0dcc31101e10b7838cd331d5e1e054
sychen303 <117107893+sychen303@users.noreply.github.com>
2024-04-19 22:16:41 +0530

    Update Login.js

--- FrontEnd/src/pages/Login.js
+++ FrontEnd/src/pages/Login.js
@@ -4,67 +4,32 @@ import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../AuthContext";
 
 function Login() {
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
-
-  const authContext = useContext(AuthContext);
-  const navigate = useNavigate();
-
-
-  const handleInputChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const authCheck = () => {
-    setTimeout(() => {
-      fetch("http://localhost:4000/api/login")
-        .then((response) => response.json())
-        .then((data) => {
-          alert("Successfully Login");
-          localStorage.setItem("user", JSON.stringify(data));
-          authContext.signin(data._id, () => {
-            navigate("/");
-          });
-        })
-        .catch((err) => {
-          alert("Wrong credentials, Try again")
-          console.log(err);
-        });
-    }, 3000);
-  };
-
-  const loginUser = (e) => {
-    // Cannot send empty data
-    if (form.email === "" || form.password === "") {
-      alert("To login user, enter details to proceed...");
-    } else {
-      fetch("http://localhost:4000/api/login", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(form),
-      })
-        .then((result) => {
-          console.log("User login", result);
-        })
-        .catch((error) => {
-          console.log("Something went wrong ", error);
-        });
-    }
-    authCheck();
-  };
-
-
-  const handleSubmit = (e) => {
+  const [Sucess,SetSuccess] = useState(false);
+  const handleSubmit = async(e) => {
     e.preventDefault();
+    const data = new FormData(e.target);
+    const FormDict = {
+      Email:data.get("email"),
+      Password:data.get("password")
+    }
+    const response = await fetch("http://localhost:5000/api/login",{
+      method:"POST",
+      body:JSON.stringify(FormDict),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    if(!response.ok){
+       SetSuccess(false);
+    }else{
+      SetSuccess(true);
+    }
   };
 
   
   return (
     <>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 h-screen  items-center place-items-center">
         <div className="flex justify-center">
           <img src={require("../assets/signup.jpg")} alt="" />
@@ -80,6 +45,7 @@ function Login() {
               Sign-in to your account
             </h2>
           </div>
+          {Sucess && <span>Server Error</span>}
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             {/* <input type="hidden" name="remember" defaultValue="true" /> */}
             <div className="-space-y-px rounded-md shadow-sm">
@@ -95,8 +61,6 @@ function Login() {
                   required
                   className="relative block w-full rounded-t-md border-0 py-1.5 px-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Email address"
-                  value={form.email}
-                  onChange={handleInputChange}
                 />
               </div>
               <div>
@@ -111,8 +75,6 @@ function Login() {
                   required
                   className="relative block w-full rounded-b-md border-0 py-1.5 px-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Password"
-                  value={form.password}
-                  onChange={handleInputChange}
                 />
               </div>
             </div>
@@ -146,7 +108,6 @@ function Login() {
               <button
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={loginUser}
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   {/* <LockClosedIcon
